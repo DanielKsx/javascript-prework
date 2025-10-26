ca86d8712cc81f9c029cd82a7a2dcbecfb57c3bf
Daniel Kowalczyk <kostex2@gmail.com>
2025-10-26 11:30:58 +0000

    Add buttons Add listeners

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
+function playGame(playerInput) {
+    clearMessages();
 // computer 
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = getMoveName(randomNumber);
-
-printMessage('Mój ruch to: ' + computerMove);
-
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('Wylosowana liczba to: ' + randomNumber);
+    let computerMove = getMoveName(randomNumber);
+    printMessage('Mój ruch to: ' + computerMove);
 // player 
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = getMoveName(playerInput);
-
-
-printMessage('Twój ruch to: ' + playerMove);
-
+   /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
+    console.log('Gracz wpisał: ' + playerInput);
+    let playerMove = getMoveName(playerInput);
+    printMessage('Twój ruch to: ' + playerMove); 
 // Result
+    displayResult (computerMove, playerMove);
+}
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+});
 
-displayResult (computerMove, playerMove);
-
-
-
-
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+});
 
 
 
